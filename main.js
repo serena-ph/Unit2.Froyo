706b80d84578b7b551fa1b8984e01a854a252baf
SerenaTPhan <pttnhann@gmail.com>
2023-08-16 18:50:55 -0500

    my brain hurts

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const total = {};
+
+const flavors = prompt ('Enter your froyo flavors seperated by commas. no number');
+const flavorsTotal = flavors.split(','); 
+for (let i=0; i<arr.length; i++){
+
+   
+
+    store(flavorsTotal)
+    console.log(total)
+
+}
